748da266c560ed7829a656ba2f37fa5d386d5142
jonytellodev <jonathantello.dev@gmail.com>
2023-07-11 01:19:31 -0300

    ejercicio1

---
+++ ejerciciostp1.js
@@ -0,0 +1,6 @@
+// 5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+// Nota: Tener en cuenta la siguiente función: parseInt
+
+let primerNumero = 4;
+let segundoNumero = 5;
+console.log(`${primerNumero} + ${segundoNumero} = ${primerNumero+segundoNumero}`);
